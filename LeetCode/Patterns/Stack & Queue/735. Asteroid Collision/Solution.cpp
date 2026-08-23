ecc75d3b9cd0f0148e3ca9a12efce49d96c84f3b
Aayushi Mishra <aayushimishra599@gmail.com>
2026-08-23 09:33:38 +0530

    Add C++ Solution to Patterns/Stack & Queue

--- LeetCode/Patterns/Stack & Queue/735. Asteroid Collision/Solution.cpp
+++ LeetCode/Patterns/Stack & Queue/735. Asteroid Collision/Solution.cpp
@@ -5,20 +5,27 @@ public:
         vector<int>st;
 
         for(int i=0;i<n;i++){
-            if(asteroids[i]>n) st.push_back(asteroids[i]);
+            if(asteroids[i]>0) st.push_back(asteroids[i]);
             else{
                 while(!st.empty() && st.back()>0 && st.back() < abs(asteroids[i])){
                     st.pop_back();
                 }
-            }
-            if(!st.empty()  &&st.back()==abs(asteroids[i]))
+                if(st.empty()) st.push_back(asteroids[i]);
+            
+            else if (!st.empty()  &&st.back()==abs(asteroids[i]))//same size
             st.pop_back();
-          else if(st.empty() || st.back()<abs(asteroids[i])){
+
+   //incoming ast is destroyed
+        else if(!st.empty() && st.back()>abs(asteroids[i])){
+            
+        }
+
+          else {
             st.push_back(asteroids[i]);
           }
-
+            }
         }
-        return asteroids;
+        return st;
         
     }
 };
