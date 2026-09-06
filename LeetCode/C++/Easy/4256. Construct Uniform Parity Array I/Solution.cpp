bbd52fe340511c12ed982ca09688a93be6abfd22
Aayushi Mishra <aayushimishra599@gmail.com>
2026-09-06 23:52:15 +0530

    Add Easy C++ Solution for 4256. Construct Uniform Parity Array I from LeetCode

--- LeetCode/C++/Easy/4256. Construct Uniform Parity Array I/Solution.cpp
+++ LeetCode/C++/Easy/4256. Construct Uniform Parity Array I/Solution.cpp
@@ -3,11 +3,21 @@ public:
     bool uniformArray(vector<int>& nums1) {
         int minEl= *min_element(nums1.begin(),nums1.end());
 
-        if(minEl%2!=0){
+        if(minEl%2==1){
             return true;     // checking odd- then we can convert all even to odd
         }
-            
+        // check if we can make all even elements in nums2
+        for(int i =0;i<nums1.size();i++){
+            if(nums1[i] %2==1)
+            return false;
+        }
+         
         return true;
 
     }
 };
+// check parity of odd and even cases
+//odd-odd = even (parity flips)
+// even - even=even
+// odd- smallest no.even = odd
+// even- odd smallest no. = odd
