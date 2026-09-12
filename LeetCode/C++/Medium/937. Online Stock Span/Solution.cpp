9162ad3e7aed8bc331a49dac3702873c4be1b5e8
Aayushi Mishra <aayushimishra599@gmail.com>
2026-09-12 18:55:27 +0530

    Add Medium C++ Solution for 937. Online Stock Span from LeetCode

--- LeetCode/C++/Medium/937. Online Stock Span/Solution.cpp
+++ LeetCode/C++/Medium/937. Online Stock Span/Solution.cpp
@@ -1,19 +1,19 @@
 class StockSpanner {
 public:
-    stack<pair<int,int>>st;
+    stack<pair<int,int>> st;
     int index = 0;
 
     int next(int price) {
 
-        while (!st.empty() && st.top.first() <= price) {
+        while (!st.empty() && st.top().first <= price) {
             st.pop();
         }
 
-        int pge = st.empty() ? -1 : st.top.second();
+        int pge = st.empty() ? -1 : st.top().second;
 
         int span = index - pge;
 
-        st.push{(index,price)};
+        st.push({price, index});
         index++;
 
         return span;
