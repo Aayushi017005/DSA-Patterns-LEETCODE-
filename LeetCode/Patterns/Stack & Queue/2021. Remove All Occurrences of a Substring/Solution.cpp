809a8f2fe567da1f76a7cce89ef9b6bd6f26dcba
Aayushi Mishra <aayushimishra599@gmail.com>
2026-09-14 15:37:53 +0530

    Add Medium C++ Solution for 2021. Remove All Occurrences of a Substring from LeetCode

--- LeetCode/Patterns/Stack & Queue/2021. Remove All Occurrences of a Substring/Solution.cpp
+++ LeetCode/Patterns/Stack & Queue/2021. Remove All Occurrences of a Substring/Solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        while(s.length()>0 && part.length<= s.length()){
+        while(s.length()>0 && part.length()<= s.length()){
             s.erase(s.find(part),part.length());
         }
         return s;
