2b2b4546216d95cc0eed3876723333d9024e684a
Aayushi Mishra <aayushimishra599@gmail.com>
2026-09-17 00:58:09 +0530

    Add Easy C++ Solution for 242. Valid Anagram from LeetCode

--- LeetCode/C++/Easy/242. Valid Anagram/Solution.cpp
+++ LeetCode/C++/Easy/242. Valid Anagram/Solution.cpp
@@ -1,11 +1,33 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()) return false;
+
+int freq[26] = [0];
+for (int i =0; i<s.length();i++){
+    freq[s[i] - 'a']++;
+    feq[t[i] - 'a']--;
+}
+     for(int i=0;i<26;i++){
+        if(freq[i] != 0) return false;
+     }
+     return true;
+    }
+};
+
+//BRUTE FORCE APPROACH
+// anagram means->
+/*Same length → same number of characters.
+Same characters with the same frequencies → order/position does not matter.*/
+// so we sort both the str and sort we checked whether both are equalor not if yes  return true else false.
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
         sort(begin(s), end(s));
         sort(t.begin(), t.end());
         if(t==s)
         return true;
-        
+
       return false;
     }
 };
