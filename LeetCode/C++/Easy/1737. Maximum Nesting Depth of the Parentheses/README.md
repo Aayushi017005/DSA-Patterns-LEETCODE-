4b40787635fda6e1b94b67c323ea51e5792ce870
Aayushi Mishra <aayushimishra599@gmail.com>
2026-09-20 14:20:53 +0530

    Add Easy C++ Solution for 1737. Maximum Nesting Depth of the Parentheses from LeetCode

--- LeetCode/C++/Easy/1737. Maximum Nesting Depth of the Parentheses/README.md
+++ LeetCode/C++/Easy/1737. Maximum Nesting Depth of the Parentheses/README.md
@@ -8,8 +8,8 @@
 String, Stack, Bracket Sequences
 
 ### 🚀 Performance
-- **Runtime:** 0 ms
-- **Memory:** 8.3 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/C++/Easy/1737. Maximum Nesting Depth of the Parentheses/Solution.cpp
+++ LeetCode/C++/Easy/1737. Maximum Nesting Depth of the Parentheses/Solution.cpp
@@ -15,4 +15,6 @@ public:
     return  ans;
     }
     
-};
+}; // this is optimal approach T(N)= O(N) and S(N)=O(1);
+// Another aproach is using stack always remember when there is a ques of valid parethesis then must be always think of stack for storing and poping parenthesis.
+// so the T(N) = o(n) and S(N)= o(n/2) { because of pushing '(' brakets are at most 1/2 length in worst case possibility} like-"((((()))))"
