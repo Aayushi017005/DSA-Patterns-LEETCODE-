9433c4f6d3b8c4391887c459f5f2e98b8832bd5e
Aayushi Mishra <aayushimishra599@gmail.com>
2026-09-21 22:46:54 +0530

    Add Medium C++ Solution for 8. String to Integer (atoi) from LeetCode

--- LeetCode/Patterns/Strings/8. String to Integer atoi/Solution.cpp
+++ LeetCode/Patterns/Strings/8. String to Integer atoi/Solution.cpp
@@ -5,8 +5,7 @@ public:
         int n = s.length()-1;
             
         for(int i=0; i<=n; i++){
-               if (s[0]=='-') num=num*-1;
-            if (s[0]=='0'|| s[0]=='-'){
+            if (i=='0'|| s[i]=='-'){
                 continue;
             }
           if(s[i]>='0' && s[i]<='9' ){
