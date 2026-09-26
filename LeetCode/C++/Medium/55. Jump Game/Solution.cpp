698f6685732dcdf4bb12d38c19bc2769bcca5447
Aayushi Mishra <aayushimishra599@gmail.com>
2026-09-26 15:11:26 +0530

    Add Medium C++ Solution for 55. Jump Game from LeetCode

--- LeetCode/C++/Medium/55. Jump Game/Solution.cpp
+++ LeetCode/C++/Medium/55. Jump Game/Solution.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-
-        int farthest = 0;
-
-        for (int i = 0; i < nums.size(); i++) {
-
-            if (i > farthest)
-                return false;
-
-            farthest = max(farthest, i + nums[i]);
-
-            if (farthest >= nums.size() - 1)
-                return true;
-        }
-
-        return true;
+       int MaxIDX=0;
+       for(int i=0;i<nums.size()-1;i++) {
+        // this condn means what if you  never reached and your i get updated becoz i++ and this is only when 0 is present in arr and ans is false;
+        if (i>MaxIDX) return false;
+        MaxIDX= max(MaxIDX, i+ nums[i]);
+       }
+       return true;
     }
 };
