b55e75137cdbd3bc69d04ffa9f20364f16e6c0a2
Aayushi Mishra <aayushimishra599@gmail.com>
2026-09-26 15:13:10 +0530

    Add Medium C++ Solution for 55. Jump Game from LeetCode

--- LeetCode/C++/Medium/55. Jump Game/Solution.cpp
+++ LeetCode/C++/Medium/55. Jump Game/Solution.cpp
@@ -4,7 +4,7 @@ public:
        int MaxIDX=0;
        for(int i=0;i<nums.size()-1;i++) {
         // this condn means what if you  never reached and your i get updated becoz i++ and this is only when 0 is present in arr and ans is false;
-        if (i>MaxIDX) return false;
+        if (i>=MaxIDX) return false;
         MaxIDX= max(MaxIDX, i+ nums[i]);
        }
        return true;
